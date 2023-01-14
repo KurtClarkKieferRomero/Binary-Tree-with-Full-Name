90a05ab5f49c43f53fbc57fc96e5de3318a60eb2
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-14 22:37:14 +0800

    Created a function that builds the binary tree with the given elements

--- Main.py
+++ Main.py
@@ -35,3 +35,27 @@ class BinarySearchTreeNode:
             if self.right:
                 return self.right.search(val)
 
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def build_tree(elements):
+        print("Building tree with these elements:",elements)
+        root = BinarySearchTreeNode(elements[0])
+
+        for i in range(1,len(elements))
+            root.add_child(elements[i])
+
+        return root
+
+
+
+
