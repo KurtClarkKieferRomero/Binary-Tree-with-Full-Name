786b1b6947bb57f7d1799780cb92cdc7e97187e7
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-15 00:19:13 +0800

    Using the max value instead of the min value for deletion

--- Main.py
+++ Main.py
@@ -85,9 +85,9 @@ class BinarySearchTreeNode:
             elif self.right is None:
                 return self.left
 
-            min_val = self.right.find_min()
-            self.data = min_val
-            self.right = self.right.delete(min_val)
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.right = self.left.delete(max_val)
 
         return self
 
