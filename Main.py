f2e18e8848ac006d6756013502472485136a029e
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-14 22:55:33 +0800

    Created a sample to test the binary search tree and fixed the function search

--- Main.py
+++ Main.py
@@ -7,9 +7,10 @@ class BinarySearchTreeNode:
     def add_child(self, data):
         if data == self.data:
             return # node already exist
+
         if data < self.data:
             if self.left:
-                self.left.add_chold(data)
+                self.left.add_child(data)
             else:
                 self.left = BinarySearchTreeNode(data)
         else:
@@ -22,8 +23,6 @@ class BinarySearchTreeNode:
     def search(self, val):
         if self.data == val:
             return True
-        else:
-            return False
 
         if val < self.data:
             if self.left:
@@ -34,6 +33,8 @@ class BinarySearchTreeNode:
         if val > self.data:
             if self.right:
                 return self.right.search(val)
+            else:
+                return False
 
     def in_order_traversal(self):
         elements = []
@@ -47,18 +48,21 @@ class BinarySearchTreeNode:
 
         return elements
 
-    def build_tree(elements):
-        print("Building tree with these elements:",elements)
-        root = BinarySearchTreeNode(elements[0])
+def build_tree(elements):
+    print("Building tree with these elements:", elements)
+    root = BinarySearchTreeNode(elements[0])
 
-        for i in range(1,len(elements))
-            root.add_child(elements[i])
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
 
-        return root
+    return root
 
-    if __name__ == '__main__':
-        countries = ["India", "Pakistan", "Germany", "USA", "China", "India", "UK", "USA"]
+if __name__ == '__main__':
+    countries = ["India", "Pakistan", "Germany", "USA", "China", "India", "UK", "USA"]
 
-        country_tree = build_tree(countries)
+    country_tree = build_tree(countries)
+
+    print("UK is in the list? ", country_tree.search("India"))
+    print("Sweden is in the list? ", country_tree.search("China"))
 
 
