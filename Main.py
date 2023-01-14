c823f510fc42f62c7218b6585652c386540703e6
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-14 23:54:29 +0800

    Created the whole delete method without the minimum value deletion

--- Main.py
+++ Main.py
@@ -77,6 +77,17 @@ class BinarySearchTreeNode:
         elif val > self.data:
             if self.right:
                 self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.Right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.right
+
+
+
+
 
 
 def build_tree(elements):
